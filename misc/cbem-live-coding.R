cbc66e8e8a1a6aa8743299e5899aeba19da689ad
David Keyes <david@rfortherestofus.com>
2025-09-22 13:10:34 -0700

    Prep for week 1

--- misc/cbem-live-coding.R
+++ misc/cbem-live-coding.R
@@ -5,19 +5,16 @@ library(ggchicklet)
 library(scales)
 library(patchwork)
 
-cbem <- read_csv("https://rin3spring2025.rfortherestofus.com/data-raw/cbem.csv")
+cbem <- read_csv("https://rin3fall2025.rfortherestofus.com/data-raw/cbem.csv")
 
 cbem_plot <- function(location_to_filter, age_group_to_filter) {
-  
-  max_y_value <- 
+  max_y_value <-
     cbem |>
     filter(location == location_to_filter) |>
-    filter(group != "All Persons") |> 
-    slice_max(order_by = percent,
-              n = 1) |> 
+    filter(group != "All Persons") |>
+    slice_max(order_by = percent, n = 1) |>
     pull(percent)
-  
-  
+
   cbem_state_average <-
     cbem |>
     filter(location == location_to_filter) |>
@@ -28,29 +25,32 @@ cbem_plot <- function(location_to_filter, age_group_to_filter) {
   cbem_state_average_text <-
     str_glue("CBEM State Rate\n{percent(cbem_state_average, accuracy = 0.1)}")
 
-
   cbem_single_state_and_age <-
     cbem |>
     filter(location == location_to_filter) |>
     filter(age_group == age_group_to_filter) |>
     filter(group != "All Persons") |>
-    mutate(group = fct(
-      group,
-      levels = c(
-        "American Indian or Alaska Native",
-        "Asian or Pacific Islander",
-        "Black or African American",
-        "White",
-        "Hispanic or Latino"
+    mutate(
+      group = fct(
+        group,
+        levels = c(
+          "American Indian or Alaska Native",
+          "Asian or Pacific Islander",
+          "Black or African American",
+          "White",
+          "Hispanic or Latino"
+        )
       )
-    )) |>
-    mutate(x_position = case_when(
-      group == "American Indian or Alaska Native" ~ 1,
-      group == "Asian or Pacific Islander" ~ 2,
-      group == "Black or African American" ~ 3,
-      group == "White" ~ 4,
-      group == "Hispanic or Latino" ~ 5.5
-    )) |>
+    ) |>
+    mutate(
+      x_position = case_when(
+        group == "American Indian or Alaska Native" ~ 1,
+        group == "Asian or Pacific Islander" ~ 2,
+        group == "Black or African American" ~ 3,
+        group == "White" ~ 4,
+        group == "Hispanic or Latino" ~ 5.5
+      )
+    ) |>
     mutate(percent_formatted = percent(percent))
 
   cbem_single_state_and_age |>
@@ -100,22 +100,20 @@ cbem_plot <- function(location_to_filter, age_group_to_filter) {
 }
 
 
-
 cbem_plot_combined <- function(state) {
   under_18_plot <-
     cbem_plot(
       location_to_filter = state,
       age_group_to_filter = "Under 18"
     )
-  
+
   under_25_plot <-
     cbem_plot(
       location_to_filter = state,
       age_group_to_filter = "Under 25"
     )
-  
+
   under_18_plot + under_25_plot
 }
 
 cbem_plot_combined(state = "New York")
-
