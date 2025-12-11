99eb82a1ba10c23b03ea7d9637ad33e6b3e9ed1f
David Keyes <david@rfortherestofus.com>
2025-12-11 13:41:13 -0800

    Update theme.R

--- misc/theme.R
+++ misc/theme.R
@@ -12,17 +12,17 @@ theme_dk <- function(hide_gridlines = FALSE, hide_legend = FALSE) {
         size = 18
       )
     )
-  
+
   if (hide_gridlines == TRUE) {
     my_theme <- my_theme +
       theme(panel.grid = element_blank())
   }
-  
+
   if (hide_legend == TRUE) {
     my_theme <- my_theme +
       theme(legend.position = "none")
   }
-  
+
   my_theme
 }
 
@@ -46,10 +46,10 @@ ggplot(
   )
 ) +
   geom_point() +
-  theme_dk(hide_legend = TRUE) 
+  theme_dk(hide_legend = TRUE)
 
-penguins |> 
-  count(island) |> 
+penguins |>
+  count(island) |>
   ggplot(
     aes(
       x = island,
@@ -57,4 +57,4 @@ penguins |>
     )
   ) +
   geom_col() +
-  theme_dk(hide_gridlines = TRUE)
+  theme_dkk(hide_gridlines = TRUE)
